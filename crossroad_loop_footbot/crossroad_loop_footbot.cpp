fbe3511d23afe264dcc08bf29e4a1311db940716
jtib <jtib@ntymail.com>
2017-05-11 13:35:54 -0400

    First commit + corrected Ivan's paths in loop files (still uglily local)

---
+++ crossroad_loop_footbot/crossroad_loop_footbot.cpp
@@ -0,0 +1,128 @@
+#include <argos3/core/utility/logging/argos_log.h>
+#include <argos3/core/utility/configuration/argos_configuration.h>
+#include <argos3/plugins/simulator/entities/box_entity.h>
+#include <QImage>
+#include "crossroad_loop_footbot.h"
+
+static const Real POV_HEIGHT = 0.2f;
+
+CCrossroadFunctionsFb::CCrossroadFunctionsFb() :
+		m_pcController(NULL),
+		m_pcEFootBot(NULL){}
+
+/****************************************/
+/****************************************/
+
+void CCrossroadFunctionsFb::Init(TConfigurationNode& t_node) {
+	m_pcEFootBot = dynamic_cast<CFootBotEntity*>(&GetSpace().GetEntity("fu0"));
+	m_pcController = &dynamic_cast<CFootBotCrossroadController&>(m_pcEFootBot->GetControllableEntity().GetController());
+}
+
+/****************************************/
+/****************************************/
+
+void CCrossroadFunctionsFb::SetPovCamera()
+{
+    m_Renderer = dynamic_cast<CQTOpenGLRender*>(&GetSimulator().GetVisualization());
+    m_Camera = &m_Renderer->GetMainWindow().GetOpenGLWidget().GetCamera();
+    CQTOpenGLWidget *m_OpenGlWidget = &m_Renderer->GetMainWindow().GetOpenGLWidget();
+    CQTOpenGLWidget::SFrameGrabData *frame = &m_OpenGlWidget->GetFrameGrabData();
+
+    // http://doc.qt.io/qt-4.8/qimage.html#bits
+    // save image
+    /*
+    m_OpenGlWidget->grabFrameBuffer().save("test.jpg", 0, frame->Quality);
+    QImage img = m_OpenGlWidget->grabFrameBuffer();
+    uchar* bits = img.bits();
+    std::cout<< "Bits: " << sizeof(bits) << " byteCount: " << img.byteCount() << std::endl;
+    */
+
+    if(m_pcController != NULL){
+    	m_pcController->img_bits = m_OpenGlWidget->grabFrameBuffer().bits();
+    	m_pcController->bytesCount = m_OpenGlWidget->grabFrameBuffer().byteCount();
+		m_pcController->bytesPerLine = m_OpenGlWidget->grabFrameBuffer().bytesPerLine();
+    }
+
+
+
+    m_CameraSettings = &m_Camera->GetActiveSettings();
+    m_SelectedEntity = dynamic_cast<CFootBotEntity*>(m_Renderer->GetMainWindow().GetOpenGLWidget().GetSelectedEntity());
+
+
+    if(m_SelectedEntity != NULL){
+        // get robot position and orientation
+        CVector3 pos_vec = m_SelectedEntity->GetEmbodiedEntity().GetOriginAnchor().Position;
+        CQuaternion orientation = m_SelectedEntity->GetEmbodiedEntity().GetOriginAnchor().Orientation;
+
+        // get robot angle
+        CRadians cZAngle, cYAngle, cXAngle;
+        orientation.ToEulerAngles(cZAngle, cYAngle, cXAngle);
+
+        // calculate camera direction vector
+        double x = pos_vec.GetX() + cos(cZAngle.GetValue());
+        double y = pos_vec.GetY() + sin(cZAngle.GetValue());
+        // fixate X so you don't get tilt
+        m_CameraSettings->Up.SetX(0);
+
+        // set position and viewpoint target
+        m_CameraSettings->Position.Set((double)(pos_vec.GetX()), (double)(pos_vec.GetY()), POV_HEIGHT);
+        m_CameraSettings->Target.Set(x, y, POV_HEIGHT);
+    }
+}
+
+/****************************************/
+/****************************************/
+
+void CCrossroadFunctionsFb::PostStep(){
+	ResetPosition();
+    SetPovCamera();
+
+    if(m_pcController != NULL){
+    	if(m_pcController->selected_robot != "none"){
+    		CFootBotEntity &cFootBot = dynamic_cast<CFootBotEntity&>(GetSpace().GetEntity(m_pcController->selected_robot));
+    		m_Renderer->GetMainWindow().GetOpenGLWidget().SelectEntity(cFootBot);
+    	    //m_Renderer = dynamic_cast<CQTOpenGLRender*>(&GetSimulator().GetVisualization());
+    	    //m_Renderer->GetMainWindow().GetOpenGLWidget().GetSelectedEntity();
+    	}
+    }
+	//CSpace::TMapPerType& boxes = GetSpace().GetEntitiesByType("box");
+	//for(CSpace::TMapPerType::iterator it = boxes.begin(); it != boxes.end(); ++it) {
+	//	CBoxEntity& box = *any_cast<CBoxEntity*>(it->second);
+		//box.EnableLEDs(med);
+	//}
+}
+
+/****************************************/
+/****************************************/
+
+void CCrossroadFunctionsFb::ResetPosition(){
+	CSpace::TMapPerType& m_cFootbots = GetSpace().GetEntitiesByType("foot-bot");
+
+	if(m_pcController != NULL){
+		m_pcController->positions_all.clear();
+	}
+
+	for(CSpace::TMapPerType::iterator it = m_cFootbots.begin(); it != m_cFootbots.end(); ++it) {
+		CFootBotEntity& cFootBot = *any_cast<CFootBotEntity*>(it->second);
+
+		CQuaternion qOrientation = cFootBot.GetEmbodiedEntity().GetOriginAnchor().Orientation;
+		CVector3 vPosition = cFootBot.GetEmbodiedEntity().GetOriginAnchor().Position;
+
+		if(m_pcController != NULL){
+	    	m_pcController->positions_all.insert(std::make_pair(cFootBot.GetId(), vPosition));
+	    }
+
+		// check up/down robots
+		if(vPosition.GetX() >= 4.7 || vPosition.GetX() <= -4.7){ // map edge
+			vPosition.SetX(vPosition.GetX() - (vPosition.GetX() * 2));
+		}
+		// check left/right robots
+		if(vPosition.GetY()>= 4.7 || vPosition.GetY() <= -4.7){
+			vPosition.SetY(vPosition.GetY() - (vPosition.GetY() * 2));
+		}
+		cFootBot.GetEmbodiedEntity().MoveTo(vPosition, qOrientation, false);
+	}
+}
+
+
+REGISTER_LOOP_FUNCTIONS(CCrossroadFunctionsFb, "crossroad_loop_footbot")
---
+++ crossroad_loop_footbot/crossroad_loop_footbot.h
@@ -0,0 +1,47 @@
+#ifndef CROSSROAD_FUNCTIONS_FB_H
+#define CROSSROAD_FUNCTIONS_FB_H
+
+#include <argos3/plugins/simulator/visualizations/qt-opengl/qtopengl_user_functions.h>
+#include <argos3/core/simulator/loop_functions.h>
+#include <argos3/core/simulator/entity/floor_entity.h>
+#include <argos3/plugins/robots/foot-bot/simulator/footbot_entity.h>
+#include <argos3/plugins/simulator/visualizations/qt-opengl/qtopengl_render.h>
+#include <argos3/plugins/simulator/visualizations/qt-opengl/qtopengl_camera.h>
+#include <argos3/plugins/simulator/visualizations/qt-opengl/qtopengl_user_functions.h>
+#include "/home/juliette/Documents/mila/plow-argos3/crossroad_footbot_controller/crossroad_footbot_controller.h"
+#include <QGLWidget>
+#include <QElapsedTimer>
+
+#ifdef __APPLE__
+#include <glu.h>
+#else
+#include <GL/glu.h>
+#endif
+
+using namespace argos;
+
+class CCrossroadFunctionsFb : public CLoopFunctions {
+
+public:
+
+   CCrossroadFunctionsFb();
+   virtual ~CCrossroadFunctionsFb() {}
+   void Init(TConfigurationNode& t_node);
+   void Update();
+   virtual void PostStep();
+
+private:
+   	void ResetPosition();
+	void SetPovCamera();
+
+	CSimulator* m_Simulator;
+
+	CQTOpenGLRender* m_Renderer;
+	CQTOpenGLCamera* m_Camera;
+	CQTOpenGLCamera::SSettings* m_CameraSettings;
+	CFootBotEntity* m_SelectedEntity;
+	CFootBotEntity* m_pcEFootBot;
+	CFootBotCrossroadController* m_pcController;
+};
+
+#endif
